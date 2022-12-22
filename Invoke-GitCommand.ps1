bc98c23ef6cb41fcd95d5665115f03ea36b082cc
kemmot <45972654+kemmot@users.noreply.github.com>
2022-12-22 15:25:31 +0000

    Fixed issues with Switch-GitWorkingCopy and added doc comments

--- Invoke-GitCommand.ps1
+++ Invoke-GitCommand.ps1
@@ -1,10 +1,34 @@
+<#
+.SYNOPSIS
+Utility function for invoking a git command.
+
+.OUTPUTS
+[String] containing the output of the git command.
+
+.PARAMETER Verb
+The branch object to switch/checkout to.
+
+.PARAMETER CommandArgs
+The name of the ref to switch/checkout to.  This can be any valid git ref such as a SHA, branch or tag name.
+
+.PARAMETER WorkingCopy
+Optional parameter allowing the working directory to be overridden before invoking the git command.
+If not specified, this will default to the current location.
+
+.EXAMPLE
+Invoke-GitCommand 'checkout' 'develop',
+
+Invokes the 'git checkout develop' command.
+
+.NOTES
+The Debug parameter can be used to output the git command generated.
+#>
 function Invoke-GitCommand() {
 	[CmdletBinding(SupportsShouldProcess=$True)]
 	Param (
 		[string] $Verb,
 		[string[]] $CommandArgs = @(''),
-		[string] $WorkingCopy = '.',
-		[switch] $What
+		[string] $WorkingCopy = '.'
 	)
 	Process {
 		$Expression = 'git'
--- Switch-GitWorkingCopy.ps1
+++ Switch-GitWorkingCopy.ps1
@@ -1,11 +1,48 @@
+<#
+.SYNOPSIS
+Switches to (checks out) a different branch in the git working copy.
+
+.OUTPUTS
+None.
+
+.PARAMETER Branch
+The branch object to switch/checkout to.
+
+.PARAMETER RefName
+The name of the ref to switch/checkout to.  This can be any valid git ref such as a SHA, branch or tag name.
+
+.PARAMETER IsTag
+Optional switch parameter that forces the ref name to be treated as a tag.
+This is useful if you have both a branch and tag of the same name when git will use the branch in preference.
+
+.PARAMETER WorkingCopy
+Optional parameter allowing the working directory to be overridden before invoking the git command.
+If not specified, this will default to the current location.
+
+.EXAMPLE
+Get-GitBranch 'develop' | Switch-GitWorkingCopy
+
+Switches to a branch returned by the Get-GitBranch function.
+
+.EXAMPLE
+Switch-GitWorkingCopy 'release-4.2' -IsTag
+
+Switches to a specified tag.
+
+.NOTES
+The Debug parameter can be used to output the git command generated.
+#>
 function Switch-GitWorkingCopy() {
+	[CmdletBinding(DefaultParameterSetName='Ref', SupportsShouldProcess=$True)]
 	Param (
-		[Parameter(Mandatory=$True, ParameterSetName='Branch', ValueFromPipeline=$True)]
+		[Parameter(Mandatory=$True, ParameterSetName='Branch', Position=1, ValueFromPipeline=$True)]
 		[GitBranch] $Branch,
-		[Parameter(Mandatory=$True, ParameterSetName='Ref')]
+		[Parameter(Mandatory=$True, ParameterSetName='Ref', Position=1)]
 		[string] $RefName,
 		[Parameter(ParameterSetName='Ref')]
 		[switch] $IsTag,
+		[Parameter(Mandatory=$False, ParameterSetName='Branch')]
+		[Parameter(Mandatory=$False, ParameterSetName='Ref')]
 		[string] $WorkingCopy = '.'
 	)
 	Process {
